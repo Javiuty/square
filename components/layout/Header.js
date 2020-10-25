4e972c1fd4c663ce4d346deebd7f3c751c1c525c
Javiuty <javiiescuadra@gmail.com>
2020-10-25 19:18:41 +0100

    getting fontawesome

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -2,27 +2,64 @@ import React from "react";
 import { css } from "@emotion/core";
 import styled from "@emotion/styled";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+
+const Title = styled.h1`
+  font-size: 6rem;
+  color: var(--color-tertiary);
+  text-transform: uppercase;
+`;
+
+const TitleSpan = styled.span`
+  color: #fff;
+  font-size: 4rem;
+`;
+
+const Logo = styled.img`
+  width: 8rem;
+  height: 8rem;
+`;
+
 const Header = () => {
   return (
     <header
       css={css`
         display: flex;
-        align-items: center;
+        justify-content: space-between;
 
-        min-height: 10vh;
-        background-color: darkblue;
+        min-height: 7vh;
+        background-color: var(--color-primary);
       `}
     >
-      <div>
-        <img src="/static/img/logo.png" alt="Logo Square" />
-        <h1>
-          s<span>quare</span>
-        </h1>
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        `}
+      >
+        <Logo src="/static/img/logo.png" alt="Logo Square" />
+        <Title>
+          s<TitleSpan>quare</TitleSpan>
+        </Title>
       </div>
-      <div>
+      <div
+        css={css`
+          display: grid;
+          grid-template-columns: repeat(2, 7rem);
+          grid-template-rows: 5rem;
+        `}
+      >
         <p>Hora</p>
         <p>Fecha</p>
-        <img src="static/img/SVG/clock.svg" />
+        <FontAwesomeIcon
+          css={css`
+            font-size: 3rem;
+            color: var(--color-tertiary);
+          `}
+          icon={faClock}
+        />
       </div>
     </header>
   );
--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -9,7 +9,8 @@ const Layout = (props) => {
         styles={css`
           :root {
             --color-primary: #0d2026;
-            --color-secondary: #034f65;
+            --color-secondary: #034a5e;
+            --color-tertiary: #0fbdf2;
           }
 
           html {
@@ -26,6 +27,7 @@ const Layout = (props) => {
           body {
             font-size: 1.6rem;
             line-height: 1.5;
+            color: #fff;
           }
         `}
       />
@@ -37,6 +39,11 @@ const Layout = (props) => {
           integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
           crossOrigin="anonymous"
         />
+        <link
+          rel="shortcut icon"
+          type="image/x-icon"
+          href="/public/favicon.ico"
+        ></link>
       </Head>
       <main>{props.children}</main>
     </>
